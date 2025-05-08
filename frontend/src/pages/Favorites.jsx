53eea60d45994e4f16f2779c2a4f3008055a89bf
VedantSachinNagare <vedantnagare25@gmail.com>
2025-05-08 22:01:46 +0530

    Initial commit: React Movie App

---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+import '../css/Favorites.css'
+
+function Favorites(){
+    return <div className="favorites-empty">
+        <h2>No favourites movies yet</h2>
+        <p>start adding movies to your favourites and they will appper here</p>
+    </div>
+}
+
+export default Favorites;
